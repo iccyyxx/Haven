c0bddd0699389046afa62f23c6954195be4333b4
iccyyxx <80975930@qq.com>
2022-10-23 13:23:47 +0800

    [feature]音乐播放器H5 Demo

---
+++ music-player/README.md
@@ -0,0 +1,6 @@
+# 这是一个音乐播放器的小界面
+借鉴了 https://vanillawebprojects.com/projects/music-player/ 这个作者的 DEMO ~
+## 遇到的问题
+### 背景铺满
+### 利用VUE动态加载 iconfont
+### 动画停止的时候保持状态
---
+++ music-player/index.js
@@ -0,0 +1,56 @@
+import { createApp } from '../vue.esm-browser.js'
+
+createApp({
+  data() {
+    return {
+      btn_pre: '&#xe603;',
+      btn_next: '&#xe602;',
+      btn_stop: '&#xea81;',
+      btn_play: '&#xea82;',
+      message: 'Hello Vue!',
+      isPlay: false,
+      width:'0',
+      nowTime:'',
+      allTime:''
+    }
+  },
+  methods: {
+    next() {
+      console.log('下一首')
+    },
+    onPre() {
+      console.log('上一首')
+    },
+    timeFormat(tim){
+      const second = String(parseInt(tim%60));
+      const minute = String(parseInt(tim/60));
+      return minute.padStart(2,'0')+":"+second.padStart(2,'0');
+    }
+  },
+  mounted() {
+    /**
+     * 获取播放文件目录
+     */
+    const curMusic = this.$refs.music
+    curMusic.onpause = () => {
+      console.log('暂停')
+    }
+    curMusic.ontimeupdate = () => {
+      this.width = String(curMusic.currentTime/curMusic.duration*100)+'%';
+      this.nowTime = this.timeFormat(curMusic.currentTime);
+      this.allTime = this.timeFormat(curMusic.duration);
+    }
+    curMusic.onplaying = (event) => {
+      // console.log(event);
+    }
+  },
+  watch:{
+    isPlay:function(cur){
+      if (cur) {
+        this.$refs.music.play()
+      }else {
+        this.$refs.music.pause()
+      }
+    }
+  }
+}).mount('#app')
